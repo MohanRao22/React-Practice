79c52de10356a6e6e87b635a018f788decd01adf
MohanRao22 <mohaneduplatform@gmail.com>
2024-01-17 15:35:50 +0530

    update-3

--- script.js
+++ script.js
@@ -8,11 +8,15 @@
  *    React.createElement("Tag", "Attributes", "Children")
  * 
  */
+
+import React from "react";
+import ReactDOM  from "react-dom/client";
+
 const parentRoot = ReactDOM.createRoot(document.getElementById("parent2"));
 const headerRoot = ReactDOM.createRoot(document.getElementById("header"));
 const desc = React.createElement("div",{id:"parent"}, React.createElement("h5",{},"This is an heading tag"));
 console.log(desc);
-const headerDesc = React.createElement("ul",{class:"nav-list"},
+const headerDesc = React.createElement("ul",{className:"nav-list"},
    [
       React.createElement("li",{},"Home"),
       React.createElement("li",{},"Services"),
