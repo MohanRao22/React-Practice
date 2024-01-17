3eed2282f9adf38109b4fc8973f34a7ba5f52d44
MohanRao22 <mohaneduplatform@gmail.com>
2024-01-17 13:11:10 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// const heading = document.createElement("h1");
+// const root = document.getElementById("parent2");
+// heading.innerHTML = "Rendered via JS";
+// root.appendChild(heading);
+
+
+/**
+ *    React.createElement("Tag", "Attributes", "Childrens")
+ * 
+ */
+const parentRoot = ReactDOM.createRoot(document.getElementById("parent2"));
+const headerRoot = ReactDOM.createRoot(document.getElementById("header"));
+const desc = React.createElement("div",{id:"parent"}, React.createElement("h5",{},"This is an heading tag"));
+console.log(desc);
+const headerDesc = React.createElement("ul",{class:"nav-list"},
+   [
+      React.createElement("li",{},"Home"),
+      React.createElement("li",{},"Services"),
+      React.createElement("li",{},"Solutions"),
+      React.createElement("li",{},"Contact"),
+   ]
+);
+console.log(headerDesc);
+
+headerRoot.render(headerDesc);
+parentRoot.render(desc);
